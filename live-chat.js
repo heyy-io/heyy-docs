98436985b77adb5655442b564acd6fd8e3daf166
Daniel Rachlin <49685515+danielR2001@users.noreply.github.com>
2025-11-06 13:18:20 +0200

    Documentation edits made through Mintlify web editor

---
+++ live-chat.js
@@ -0,0 +1,18 @@
+  <script type="text/javascript">
+    function loadHeyy() {
+      window.heyySettings = { widgetId: "JlIknkx9lv7L117y", baseUrl: "http://localhost:3050" };
+
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://assets.heyy.io/live-chat-dev/live-chat.js';
+      document.head.appendChild(s);
+    }
+    window.addEventListener('load', function () {
+      if ('requestIdleCallback' in window) {
+        requestIdleCallback(loadHeyy);
+      } else {
+        setTimeout(loadHeyy, 3000);
+      }
+    });
+  </script>
